ed231cc0c502bbae3fcbd68dee683c16088b38b1
Naveenkumar Buddanagoudar <naveenkumar@fairprice.com.sg>
2019-11-12 23:38:56 +0800

    Added test case fot bird walk, fly and swim

---
+++ src/test/java/com/singtel/assignment/model/BirdTest.java
@@ -0,0 +1,53 @@
+package com.singtel.assignment.model;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class BirdTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+    private final PrintStream originalOut = System.out;
+    private final PrintStream originalErr = System.err;
+
+    @BeforeEach
+    public void setUpStreams() {
+        System.setOut(new PrintStream(outContent));
+        System.setErr(new PrintStream(errContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+        System.setErr(originalErr);
+    }
+
+    @Test
+    public void testBirdWalk() {
+        Bird bird = new Bird();
+        bird.walk();
+
+        Assertions.assertThat(outContent.toString()).isEqualToIgnoringNewLines("I am walking");
+    }
+
+    @Test
+    public void testBirdSing() {
+        Bird bird = new Bird();
+        bird.sing();
+
+        Assertions.assertThat(outContent.toString()).isEqualToIgnoringNewLines("I am singing");
+    }
+
+    @Test
+    public void testBirdFly() {
+        Bird bird = new Bird();
+        bird.fly();
+
+        Assertions.assertThat(outContent.toString()).isEqualToIgnoringNewLines("I am flying");
+    }
+}
