c58ee4e3d00d15e5d8031ccd97c86e08df2633bc
Naveenkumar Buddanagoudar <naveenkumar@fairprice.com.sg>
2019-11-12 23:25:22 +0800

    Initial Given setup

--- src/main/java/com/singtel/assignment/Application.java
+++ src/main/java/com/singtel/assignment/Application.java
@@ -1,7 +1,10 @@
 package com.singtel.assignment;
 
+import com.singtel.assignment.model.Bird;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class Application {
@@ -10,4 +13,13 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@EventListener(ApplicationReadyEvent.class)
+	public void doSomethingAfterStartup() {
+		System.out.println("Application started");
+
+		Bird bird = new Bird();
+		bird.walk();
+		bird.fly();
+		bird.sing();
+	}
 }
